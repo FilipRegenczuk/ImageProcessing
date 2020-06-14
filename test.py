da702937da6dcd98a905d9eef0ef730e59b912bd
FilipRegenczuk <f.regenczuk@outlook.com>
2020-06-14 10:36:36 +0200

    edges processing

--- test.py
+++ test.py
@@ -2,7 +2,22 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./images/forest.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('./images/rtg_chest.jpg', cv2.IMREAD_GRAYSCALE)
 
-plt.imshow(img), plt.axis("off")
+median_intensity = np.median(img)
+lower_threshold = int(max(0, (1.0 - 0.4) * median_intensity))
+upper_threshold = int(min(255, (1.0 + 0.4) * median_intensity))
+img_canny = cv2.Canny(img, lower_threshold, upper_threshold)
+
+
+
+plt.figure()
+plt.imshow(img, cmap='gray'), plt.axis("off")
+plt.title("Orginal image")
+
+plt.figure()
+plt.imshow(img_canny, cmap='gray'), plt.axis("off")
+plt.title("Edges")
+
+
 plt.show()
