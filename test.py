84c2a4bc75497fea6b199673e468ac5b16433596
FilipRegenczuk <f.regenczuk@outlook.com>
2020-06-14 09:31:45 +0200

    some edit

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-# it is a test
+import numpy as np
+from matplotlib import pyplot as plt, cm
+
+random_img = np.random.rand(500, 1500)
+plt.imshow(random_img, cmap=cm.gray, interpolation="nearest")
+plt.show()
