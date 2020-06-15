8e3f5059b001328cecd1aedb1df17a737002e735
FilipRegenczuk <f.regenczuk@outlook.com>
2020-06-15 16:39:32 +0200

    adding binarized img

--- test.py
+++ test.py
@@ -4,20 +4,31 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('./images/rtg_chest.jpg', cv2.IMREAD_GRAYSCALE)
 
-median_intensity = np.median(img)
-lower_threshold = int(max(0, (1.0 - 0.4) * median_intensity))
-upper_threshold = int(min(255, (1.0 + 0.4) * median_intensity))
-img_canny = cv2.Canny(img, lower_threshold, upper_threshold)
+# Wykrycie krawedzi
+# median_intensity = np.median(img)
+# lower_threshold = int(max(0, (1.0 - 0.4) * median_intensity))
+# upper_threshold = int(min(255, (1.0 + 0.4) * median_intensity))
+# img_canny = cv2.Canny(img, lower_threshold, upper_threshold)
+
+
+# Binaryzowanie obrazu
+# max_output_value = 255
+# neighborhood_size = 99
+# subtract_from_mean = 10
+# img_binarized = cv2.adaptiveThreshold(img, max_output_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, neighborhood_size, subtract_from_mean)
 
 
 
-plt.figure()
-plt.imshow(img, cmap='gray'), plt.axis("off")
-plt.title("Orginal image")
 
-plt.figure()
-plt.imshow(img_canny, cmap='gray'), plt.axis("off")
-plt.title("Edges")
+plt.imshow(img, cmap="gray"), plt.axis("off")
+plt.title("Orginal")
 
+# plt.figure()
+# plt.imshow(img_canny, cmap="gray"), plt.axis("off")
+# plt.title("Edges")
+
+# plt.figure()
+# plt.imshow(img_binarized, cmap="gray"), plt.axis("off")
+# plt.title("Binarized")
 
 plt.show()
