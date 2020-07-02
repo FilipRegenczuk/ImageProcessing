1ce40c5d82055ad8a71abdc55b2f1c947e6d2782
FilipRegenczuk <f.regenczuk@outlook.com>
2020-07-02 08:36:53 +0200

    testing gamma correction

--- terminal_app.py
+++ terminal_app.py
@@ -1,7 +1,10 @@
 import cv2
-import numpy as np
 import sys
+import numpy as np
+
 from matplotlib import pyplot as plt
+from skimage import exposure
+
 
 def interface():
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from skimage import exposure
 
 
 img = cv2.imread('./images/rtg_palm.jpg', cv2.IMREAD_GRAYSCALE)
@@ -8,11 +9,15 @@ img = cv2.imread('./images/rtg_palm.jpg', cv2.IMREAD_GRAYSCALE)
 
 
 print(img)
-
-img =  * img
+img = 1 * img
 print(img)
 plt.imshow(img, cmap="gray"), plt.axis("off")
 plt.title("Orginal")
+
+plt.figure()
+gamma_corrected = exposure.adjust_gamma(img, 2)
+plt.imshow(gamma_corrected, cmap="gray"), plt.axis("off")
+plt.title("Gamma corrected")
 
 # plt.figure()
 # plt.imshow(img_canny, cmap="gray"), plt.axis("off")
